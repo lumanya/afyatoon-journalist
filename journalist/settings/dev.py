9391318a6956c606d4c67d7bc4a8bf8bbfb40367
lumanya <shaibulumanya@gmail.com>
2024-10-07 16:59:37 +0300

    [Initialize] project

---
+++ journalist/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!d%-9p4bawyfw8%!c$l5=x_1lme_i+2u1-^@ads7dyqd7im#m("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
